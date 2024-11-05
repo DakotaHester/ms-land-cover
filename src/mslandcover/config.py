104c40dc222dd48049158406266328e12e093ef7
Dakota Hester <admin@dakotahester.com>
2024-11-05 09:15:00 -0600

    Started sampling procedure

--- src/mslandcover/config.py
+++ src/mslandcover/config.py
@@ -1,4 +1,32 @@
+from .utils import hex_to_rgb
+
 # Mississippi Transverse Mercator projection parameters (https://epsg.io/3813)
 MSTM_PROJ4 = '+proj=tmerc +lat_0=32.5 +lon_0=-89.75 +k=0.9998335 +x_0=500000 +y_0=1300000'
 MSTM_WKT = 'PROJECTION["Transverse_Mercator"],PARAMETER["latitude_of_origin",32.5],PARAMETER["central_meridian",-89.75],PARAMETER["scale_factor",0.9998335],PARAMETER["false_easting",500000],PARAMETER["false_northing",1300000]'
 MSTM_EPSG = '3813'
+
+LEGEND_CLASSES = {
+    0: 'Nodata',
+    1: 'Water',
+    2: 'Developed',
+    3: 'Barren',
+    4: 'Forest',
+    5: 'Shrubland',
+    6: 'Herbaceous',
+    7: 'Cultivated',
+    8: 'Wetlands',
+}
+
+LEGEND_COLORS_HEX = {
+    0: '#000000',
+    1: '#4A6A9E',
+    2: '#FF0000',
+    3: '#CCC6B8',
+    4: '#317645',
+    5: '#A68B34',
+    6: '#99C547',
+    7: '#FBF452',
+    8: '#C0E2F7',
+}
+
+LEGEND_COLORS_RGB = {k: hex_to_rgb(v) for k, v in LEGEND_COLORS_HEX.items()}
--- src/mslandcover/utils.py
+++ src/mslandcover/utils.py
@@ -1,4 +1,5 @@
 import os
+from typing import Tuple
 
 def raise_if_not_exists(path: str) -> None:
     '''
@@ -20,3 +21,21 @@ def raise_if_not_exists(path: str) -> None:
     '''
     if not os.path.exists(path):
         raise FileNotFoundError(f'The path {path} does not exist.')
+
+def hex_to_rgb(hex_color: str) -> Tuple[int, int, int]:
+    '''
+    Convert a hex color to an RGB tuple.
+    
+    Parameters
+    ----------
+    hex_color : str
+        The hex color to convert.
+    
+    Returns
+    -------
+    Tuple[int, int, int]
+        The RGB tuple.
+    '''
+    if hex_color.startswith('#'):
+        hex_color = hex_color[1:]
+    return tuple(int(hex_color[i:i+2], 16) for i in (0, 2, 4))
