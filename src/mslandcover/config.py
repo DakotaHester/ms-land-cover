f222c243d2998b46166aaf1b4eee996dfa7b82f3
Dakota Hester <admin@dakotahester.com>
2024-11-06 10:37:42 -0600

    sampled some tiles for testing, created script for roboflow annotation

--- src/mslandcover/config.py
+++ src/mslandcover/config.py
@@ -5,7 +5,7 @@ MSTM_PROJ4 = '+proj=tmerc +lat_0=32.5 +lon_0=-89.75 +k=0.9998335 +x_0=500000 +y_
 MSTM_WKT = 'PROJECTION["Transverse_Mercator"],PARAMETER["latitude_of_origin",32.5],PARAMETER["central_meridian",-89.75],PARAMETER["scale_factor",0.9998335],PARAMETER["false_easting",500000],PARAMETER["false_northing",1300000]'
 MSTM_EPSG = '3813'
 
-LEGEND_CLASSES = {
+NLCD_LEVEL_I_LEGEND_CLASSES = {
     0: 'Nodata',
     1: 'Water',
     2: 'Developed',
@@ -17,7 +17,7 @@ LEGEND_CLASSES = {
     8: 'Wetlands',
 }
 
-LEGEND_COLORS_HEX = {
+NLCD_LEVEL_I_LEGEND_COLORS_HEX = {
     0: '#000000',
     1: '#4A6A9E',
     2: '#FF0000',
@@ -29,4 +29,17 @@ LEGEND_COLORS_HEX = {
     8: '#C0E2F7',
 }
 
-LEGEND_COLORS_RGB = {k: hex_to_rgb(v) for k, v in LEGEND_COLORS_HEX.items()}
+NLCD_LEVEL_I_LEGEND_COLORS_RGB = {k: hex_to_rgb(v) for k, v in NLCD_LEVEL_I_LEGEND_COLORS_HEX.items()}
+
+MS_LEGEND_CLASSES = {
+    0: 'Nodata',
+    1: 'Open Water',
+    2: 'Impervious (raised structures)',
+    3: 'Impervious (paved surfaces)',
+    4: 'Barren Land',
+    5: 'Forest',
+    6: 'Herbaceous',
+    7: 'Cultivated Crops',
+    8: 'Wetlands',
+    9: 'Unclassified',
+}
