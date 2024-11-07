66c3399457772e1e97a2d32308403b02e41599af
Dakota Hester <admin@dakotahester.com>
2024-11-07 11:52:10 -0600

    Syncing changes

--- src/mslandcover/config.py
+++ src/mslandcover/config.py
@@ -5,7 +5,7 @@ MSTM_PROJ4 = '+proj=tmerc +lat_0=32.5 +lon_0=-89.75 +k=0.9998335 +x_0=500000 +y_
 MSTM_WKT = 'PROJECTION["Transverse_Mercator"],PARAMETER["latitude_of_origin",32.5],PARAMETER["central_meridian",-89.75],PARAMETER["scale_factor",0.9998335],PARAMETER["false_easting",500000],PARAMETER["false_northing",1300000]'
 MSTM_EPSG = '3813'
 
-MS_LEGEND_CLASSES = {
+LEGEND_CLASSES = {
     0: 'Nodata',
     1: 'Open Water',
     2: 'Impervious (raised structures)',
@@ -17,7 +17,7 @@ MS_LEGEND_CLASSES = {
     8: 'Unclassified',
 }
 
-MS_LEGEND_COLORS_HEX = {
+LEGEND_COLORS_HEX = {
     0: '#ffffff',
     1: '#516c97',
     2: '#989899',
@@ -29,4 +29,4 @@ MS_LEGEND_COLORS_HEX = {
     8: '#000000',
 }
 
-MS_LEGEND_COLORS_RGB = {k: hex_to_rgb(v) for k, v in MS_LEGEND_COLORS_HEX.items()}
+LEGEND_COLORS_RGB = {k: hex_to_rgb(v) for k, v in LEGEND_COLORS_HEX.items()}
