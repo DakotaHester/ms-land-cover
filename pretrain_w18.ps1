667c97a40c6b8ab6326d86652809d244723cd8d9
Dakota Hester <admin@dakotahester.com>
2025-01-02 16:15:59 -0600

    Fix broken HSV reconstruction

--- pretrain_w18.ps1
+++ pretrain_w18.ps1
@@ -1,6 +1,6 @@
 
 $models = @("hrnet_w18")
-$schemes = @("simclr", "dae_simclr", "hsv_simclr", "dae_hsv_simclr")
+$schemes = @("simclr", "dae_simclr", "hsv_simclr", "dae_hsv_simclr" "ae_simclr")
 
 foreach ($model in $models) {
     foreach ($scheme in $schemes) {
