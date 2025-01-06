45790fe036be1496a240960cc0bd46376feb8928
Dakota Hester <admin@dakotahester.com>
2025-01-06 14:02:05 -0600

    Fix multi-loss routine

--- pretrain.py
+++ pretrain.py
@@ -547,8 +547,8 @@ def main():
                         tqdm_postfix['MSE Loss'] = f'{epoch_reconstruction_loss:.2e}'
                     
                     if is_multitask:
-                        loss = reconstruction_loss_weight * reconstruction_loss + contrastive_loss
-                        total_loss_values.append(loss.item())
+                        loss = [reconstruction_loss_weight * reconstruction_loss, contrastive_loss]
+                        total_loss_values.append(sum([l.item() for l in loss]))
                         epoch_loss = np.sum(total_loss_values) / ((step * args.mini_batch_size) + len(batch))
                         tqdm_postfix['Total Loss'] = f'{epoch_loss:.2e}'
                     
