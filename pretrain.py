d784e2e8324db0c7d815405b41a5fab770da45f8
Dakota Hester <admin@dakotahester.com>
2025-01-06 13:53:11 -0600

    Debugging pretrain code

--- pretrain.py
+++ pretrain.py
@@ -546,7 +546,7 @@ def main():
                         tqdm_postfix['MSE Loss'] = f'{epoch_reconstruction_loss:.2e}'
                     
                     if is_multitask:
-                        loss = reconstruction_loss_weight * reconstruction_loss + contrastive_loss
+                        loss = (reconstruction_loss_weight * reconstruction_loss) + contrastive_loss
                         total_loss_values.append(loss.item())
                         epoch_loss = np.sum(total_loss_values) / ((step * args.mini_batch_size) + len(batch))
                         tqdm_postfix['Total Loss'] = f'{epoch_loss:.2e}'
