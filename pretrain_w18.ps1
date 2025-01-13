bd3be2a02406cde9984eb0bc9a57e3c82d586f48
Dakota Hester <admin@dakotahester.com>
2025-01-12 18:19:38 -0600

    Support for varying image sizes during pretraining

--- pretrain_w18.ps1
+++ pretrain_w18.ps1
@@ -1,9 +1,9 @@
 
-$models = @("hrnet_w18")
-$schemes = @("simclr", "dae_simclr", "hsv_simclr", "dae_hsv_simclr" "ae_simclr")
+$models = @("resnet152")
+$schemes = @("ae", "dae", "hsv", "dae_hsv", "simclr", "dae_simclr", "hsv_simclr", "dae_hsv_simclr", "ae_simclr")
 
 foreach ($model in $models) {
     foreach ($scheme in $schemes) {
-        python pretrain.py --model $model --pretrain_scheme $scheme --pretrain_hdf5_path .\data\splits\pretrain.hdf5
+        python pretrain.py --model $model --pretrain_scheme $scheme --pretrain_hdf5_path .\data\splits\pretrain.hdf5 --num_epochs 10 --image_size 48 --mini_batch_size 128
     }
 }
