e49497faa2b656d1ba222df4c88065cc2752e330
Dakota Hester <admin@dakotahester.com>
2025-01-12 19:15:15 -0600

    PLay around with default params

--- pretrain_w18.ps1
+++ pretrain_w18.ps1
@@ -1,9 +1,9 @@
 
 $models = @("resnet152")
-$schemes = @("ae", "dae", "hsv", "dae_hsv", "simclr", "dae_simclr", "hsv_simclr", "dae_hsv_simclr", "ae_simclr")
+$schemes = @("dae", "hsv", "dae_hsv", "ae")
 
 foreach ($model in $models) {
     foreach ($scheme in $schemes) {
-        python pretrain.py --model $model --pretrain_scheme $scheme --pretrain_hdf5_path .\data\splits\pretrain.hdf5 --num_epochs 10 --image_size 48 --mini_batch_size 128
+        python pretrain.py --model $model --pretrain_scheme $scheme --pretrain_hdf5_path .\data\splits\pretrain.hdf5 --num_epochs 5 --image_size 96 --mini_batch_size 32
     }
 }
