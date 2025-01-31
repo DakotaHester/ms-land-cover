b4cdc8be25ea33204a2046a746842ca4dcb4af19
Dakota Hester <admin@dakotahester.com>
2025-01-31 16:01:09 -0600

    Add randinit scenario to model pretraining tests

--- submit_pretrain_jobs_pbs.sh
+++ submit_pretrain_jobs_pbs.sh
@@ -21,23 +21,37 @@ mkdir -p "$LOG_DIR"
 
 for model in unet
 do
-	for frozen_encoder in 1
+	for frozen_encoder in 0 # skip 1 as already submitted
 	do
-		for pretrain_scheme in dae lab dae_lab
+		for randinit in 1 # skip 0 as already submitted
 		do
-			if [ "$frozen_encoder" = "1" ]; then
-				FROZEN_ENCODER_FLAG="--frozen_encoder"
-				JOB_NAME="${NAME}_${model}_${pretrain_scheme}_frozen"
+
+			if [ "$randinit" = "1" ]; then
+				if [ "$frozen_encoder" = "1" ]; then
+					continue
+				fi
+				RANDINIT_FLAG="--randinit"
+				JOB_NAME="${NAME}_${model}_randinit"
 			else
-				FROZEN_ENCODER_FLAG=""
-				JOB_NAME="${NAME}_${model}_${pretrain_scheme}"
+				RANDINIT_FLAG=""
+				JOB_NAME="${NAME}_${model}"
 			fi
-			
 
-			PBS_SCRIPT="./pbs_scripts/${JOB_NAME}.pbs"
-			mkdir -p "$(dirname "$PBS_SCRIPT")"
+			for pretrain_scheme in dae lab dae_lab
+			do
+				if [ "$frozen_encoder" = "1" ]; then
+					FROZEN_ENCODER_FLAG="--frozen_encoder"
+					JOB_NAME="${NAME}_${model}_${pretrain_scheme}_frozen"
+				else
+					FROZEN_ENCODER_FLAG=""
+					JOB_NAME="${NAME}_${model}_${pretrain_scheme}"
+				fi
+				
 
-			cat > "$PBS_SCRIPT" <<EOL
+				PBS_SCRIPT="./pbs_scripts/${JOB_NAME}.pbs"
+				mkdir -p "$(dirname "$PBS_SCRIPT")"
+
+				cat > "$PBS_SCRIPT" <<EOL
 #!/bin/bash
 #PBS -N $JOB_NAME
 #PBS -q $QUEUE
@@ -56,9 +70,10 @@ module load python
 conda init
 source ~/.bashrc
 conda activate mslc
-python $SCRIPT_NAME --model $model --pretrain_scheme $pretrain_scheme --pretrain_hdf5_path $HDF5_PATH --mini_batch_size $MINI_BATCH_SIZE --full_batch_size $FULL_BATCH_SIZE --num_workers $NCPUS --learning_rate_factor $LEARNING_RATE_FACTOR $FROZEN_ENCODER_FLAG
+python $SCRIPT_NAME --model $model --pretrain_scheme $pretrain_scheme --pretrain_hdf5_path $HDF5_PATH --mini_batch_size $MINI_BATCH_SIZE --full_batch_size $FULL_BATCH_SIZE --num_workers $NCPUS --learning_rate_factor $LEARNING_RATE_FACTOR $FROZEN_ENCODER_FLAG $RANDINIT_FLAG
 EOL
-		    qsub "$PBS_SCRIPT"
+		    	qsub "$PBS_SCRIPT"
+			done
 	    done
     done
 done
