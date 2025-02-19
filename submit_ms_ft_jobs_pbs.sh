35db284251412da81fff4a799814d5d89b39e2be
Dakota Hester <admin@dakotahester.com>
2025-02-19 16:23:19 -0600

    More tweaks to msft1 submission

--- submit_ms_ft_jobs_pbs.sh
+++ submit_ms_ft_jobs_pbs.sh
@@ -52,7 +52,12 @@ for pretrain_scheme in "${pretrain_schemes[@]}"; do
                 continue
             fi
 
-            JOB_NAME="resunet_${pretrain_scheme}_ft_stage1${freeze_encoder}"
+            JOB_NAME="resunet_${pretrain_scheme}_ft_stage1${freeze_encoder}_{$ft_data##*/}"
+            if [[ "$ft_data" == *"cpb_tests"* ]]; then
+                JOB_NAME="${JOB_NAME}_cpb"
+            else
+                JOB_NAME="${JOB_NAME}_mslc"
+            fi
                                     
             # Create a PBS script for the job
             PBS_SCRIPT="./pbs_scripts/${JOB_NAME}.pbs"
