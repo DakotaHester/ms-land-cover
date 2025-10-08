3a7ee2f77aa206bfc2b07aacb00db8e4bf478cb1
Dakota Hester <admin@dakotahester.com>
2025-10-08 10:44:49 -0500

    Merge model changes

--- finetune.py
+++ finetune.py
@@ -18,7 +18,7 @@ from mslandcover.utils import Logger, get_torch_device, ProfilerHistory, load_pt
 from mslandcover.data.datasets import FineTuneDataset
 from mslandcover.data.transforms import StandardDataAugmentations 
 from mslandcover.loss import FocalLoss
-from mslandcover.models import DeepLabV3Plus, ResNetBackbone, UNet, AttentionUNet, ResNetBackboneUNet, SimpleLinearProbingResNet, MultiScaleLinearProbingResNet, UPerNet, PSPNet, BiSeNet, DANet, PAN
+from mslandcover.models import DeepLabV3Plus, ResNetBackbone, UNet, AttentionUNet, ResNetBackboneUNet, SimpleLinearProbingResNet, MultiScaleLinearProbingResNet, UPerNet, PSPNet, BiSeNet, DANet, PAN, FCN
 from mslandcover import metrics
 
 
@@ -28,7 +28,7 @@ def parse_arguments() -> argparse.Namespace:
     parser.add_argument(
         '--model',
         type=str,
-        choices=['deeplabv3plus', 'unet', 'attention_unet', 'linear_probe', 'multiscale_linear_probe', 'upernet', 'pspnet', 'bisenet', 'danet', 'pan'],
+        choices=['deeplabv3plus', 'unet', 'attention_unet', 'linear_probe', 'multiscale_linear_probe', 'upernet', 'pspnet', 'bisenet', 'danet', 'pan', 'fcn'],
         default='linear_probe',
         help='The model to use for training',
     )
@@ -407,7 +407,7 @@ def main() -> None:
         model = model.to(device)
     
     
-    elif args.model in ['unet', 'attention_unet', 'multiscale_linear_probe', 'linear_probe', 'upernet', 'bisenet']:
+    elif args.model in ['unet', 'attention_unet', 'multiscale_linear_probe', 'linear_probe', 'upernet', 'bisenet', 'fcn']:
         
         backbone = ResNetBackboneUNet(
             in_channels=args.n_bands,
@@ -455,7 +455,9 @@ def main() -> None:
             model = UPerNet(backbone=backbone, num_classes=num_classes)
         elif args.model == 'bisenet':
             model = BiSeNet(backbone=backbone, num_classes=num_classes)
-        
+        elif args.model == 'fcn':
+            model = FCN(backbone=backbone, num_classes=num_clases)
+            
         model = model.to(device)
     
     else:
